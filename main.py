6e8990010a61a552bbb36028829bbd4e6819dd43
felixshier <45214731+felixshier@users.noreply.github.com>
2021-11-28 17:03:01 -0500

    update main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from functions import *
+
+stock = 5
+
+cost, optimalOrders = DP_costFunction(stock)
+print("stock: " + str(stock))
+print("optimal orders: " + str(optimalOrders))
+print("cost: " + str(cost))
+
+
+
 
